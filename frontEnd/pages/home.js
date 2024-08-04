562038eb6fea49e28b27a9cbc4f2cf5afb726eb3
Mazen Youssef <98108192+Mazen1004@users.noreply.github.com>
2024-08-03 23:21:07 -0400

    Revert "artist page"
    This reverts commit f6c8b50312bd3b2ec71f8d016ad93f0963c81bfd, reversing
    changes made to b62ab98f19092b8e777372849dd7b2b1ffd99466.

--- frontEnd/pages/home.js
+++ frontEnd/pages/home.js
@@ -48,7 +48,6 @@ export default function Home({ token, setToken }) {
 
   useEffect(() => {
     async function fetchProfile() {
-      console.log(token);
       try {
         const response = await fetch("https://api.spotify.com/v1/me", {
           headers: {
@@ -120,7 +119,7 @@ export default function Home({ token, setToken }) {
   const handleCreateRoom = async () => {
     try {
       const response = await fetch(
-        `${process.env.EXPO_PUBLIC_API_URL}/create_room`,
+        "https://hackthe6ix.onrender.com/create_room",
         {
           method: "POST",
           headers: {
@@ -135,8 +134,8 @@ export default function Home({ token, setToken }) {
         }
       );
       const data = await response.json();
-      console.log(data);
       navigation.navigate("Room", { roomId: data.room_id, token, profile });
+      setModalVisible(false);
     } catch (error) {
       console.error("Error creating room:", error);
     }
