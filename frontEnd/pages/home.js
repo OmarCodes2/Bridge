b786ffcad13b4e36546950d7c8e1a006f414fd23
OmarCodes2 <omarcodes2@gmail.com>
2024-08-04 03:47:04 -0400

    home.js changes

--- frontEnd/pages/home.js
+++ frontEnd/pages/home.js
@@ -15,6 +15,73 @@ const formatNumberWithSpaces = (number) => {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 };
 
+const modes = [
+  {
+    type: 'artist',
+    image: require('../assets/artist.png'),
+    id: null,
+    search: true,
+    insert: false,
+    description: 'How well do you know your favourite artist?'
+  },
+  {
+    type: 'friend',
+    image: require('../assets/friend.png'),
+    id: null,
+    search: false,
+    insert: false,
+    description: 'A test based on you and your friends music tastes.'
+  },
+  {
+    type: 'playlist',
+    image: require('../assets/playlist.png'),
+    id: null,
+    search: false,
+    insert: true,
+    description: 'How well do you know a playlist?'
+  },
+  {
+    type: 'playlist',
+    image: require('../assets/kpop.png'),
+    id: '3Ir5YWemOTGRRfXgROrsDV',
+    search: false,
+    insert: false,
+    description: 'For all our Kpop stans.'
+  },
+  {
+    type: 'playlist',
+    image: require('../assets/rock.png'),
+    id: '37i9dQZF1DWXRqgorJj26U',
+    search: false,
+    insert: false,
+    description: 'Test your Rock knowledge!'
+  },
+  {
+    type: 'playlist',
+    image: require('../assets/pop.png'),
+    id: '34NbomaTu7YuOYnky8nLXL',
+    search: false,
+    insert: false,
+    description: 'Do you know your pop hits?'
+  },
+  {
+    type: 'playlist',
+    image: require('../assets/rap.png'),
+    id: '37i9dQZF1DX0XUsuxWHRQd',
+    search: false,
+    insert: false,
+    description: 'How good is your rap game?'
+  },
+  {
+    type: 'playlist',
+    image: require('../assets/anime.png'),
+    id: '1YA5cPIfDy3L03bGnNiDM7',
+    search: false,
+    insert: false,
+    description: 'For all the weebs!'
+  }
+];
+
 export default function Home({ token, setToken }) {
   const [profile, setProfile] = useState(null);
   const [isModalVisible, setModalVisible] = useState(false);
@@ -23,28 +90,13 @@ export default function Home({ token, setToken }) {
   const [artistImage, setArtistImage] = useState(null);
   const [artistName, setArtistName] = useState("");
   const [artistFollowers, setArtistFollowers] = useState("");
-  const [artistId, setArtistId] = useState(""); // New state for artist ID
+  const [artistId, setArtistId] = useState("");
   const [questionType, setQuestionType] = useState("");
+  const [playlistLink, setPlaylistLink] = useState("");
+  const [playlistName, setPlaylistName] = useState("");
+  const [playlistImage, setPlaylistImage] = useState("");
+  const [playlistOwner, setPlaylistOwner] = useState(""); // New state for playlist owner
   const navigation = useNavigation();
-
-  const gridItems = [
-    {
-      id: 1,
-      title: "Item 1",
-    },
-    {
-      id: 2,
-      title: "Item 2",
-    },
-    {
-      id: 3,
-      title: "Item 3",
-    },
-    {
-      id: 4,
-      title: "Item 4",
-    },
-  ];
 
   useEffect(() => {
     async function fetchProfile() {
@@ -71,6 +123,12 @@ export default function Home({ token, setToken }) {
     }
   }, [artist]);
 
+  useEffect(() => {
+    if (selectedItem && selectedItem.insert && playlistLink.trim() !== "") {
+      fetchPlaylistDetails();
+    }
+  }, [playlistLink]);
+
   const fetchArtistImage = async () => {
     try {
       const response = await fetch(
@@ -96,20 +154,49 @@ export default function Home({ token, setToken }) {
         setArtistName(artistItem.name || "");
         setArtistFollowers(
           formatNumberWithSpaces(artistItem.followers.total) || ""
-        ); // Format
-        setArtistId(artistItem.id || ""); // Set artist ID
+        );
+        setArtistId(artistItem.id || "");
       } else {
         setArtistImage(null);
         setArtistName("");
         setArtistFollowers("");
-        setArtistId(""); // Reset artist ID if not found
+        setArtistId("");
       }
     } catch (error) {
       console.error("Error fetching artist image:", error);
       setArtistImage(null);
       setArtistName("");
       setArtistFollowers("");
-      setArtistId(""); // Reset artist ID on error
+      setArtistId("");
+    }
+  };
+
+  const fetchPlaylistDetails = async () => {
+    const playlistId = playlistLink.split('playlist/')[1].split('?')[0];
+    try {
+      const response = await fetch(
+        `https://api.spotify.com/v1/playlists/${playlistId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      setPlaylistName(data.name);
+      setPlaylistImage(data.images[0]?.url || null);
+      setPlaylistOwner(data.owner.display_name || ""); // Set playlist owner
+      setArtistId(playlistId); // Set the playlist ID as artistId
+    } catch (error) {
+      console.error("Error fetching playlist details:", error);
+      setPlaylistName("");
+      setPlaylistImage(null);
+      setPlaylistOwner("");
     }
   };
 
@@ -118,6 +205,8 @@ export default function Home({ token, setToken }) {
   };
 
   const handleCreateRoom = async () => {
+    const roomObject = selectedItem.type === 'artist' ? { artist: artistId, album: "" } : selectedItem.type === 'friend' ? { artist: "", album: "" } : { artist: artistId, album: "" };
+
     try {
       const response = await fetch(
         `${process.env.EXPO_PUBLIC_API_URL}/create_room`,
@@ -127,10 +216,10 @@ export default function Home({ token, setToken }) {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            'token':token,
-            'question_type': "hello",
-            'quiz_type': "artist",
-            'room_object': {'artist': artistId, 'album': ""}
+            token: token,
+            question_type: "hello",
+            quiz_type: selectedItem.type,
+            room_object: roomObject,
           }),
         }
       );
@@ -146,8 +235,9 @@ export default function Home({ token, setToken }) {
   const handleJoinRoom = () => {
     navigation.navigate("SearchRoom", { token, profile });
   };
+
   const handleStartGame = () => {
-    navigation.navigate("Start"); // Navigate to Start page
+    navigation.navigate("Start");
   };
 
   const toggleModal = (item) => {
@@ -173,13 +263,13 @@ export default function Home({ token, setToken }) {
       )}
       {/* Grid Layout */}
       <View style={styles.gridContainer}>
-        {gridItems.map((item) => (
+        {modes.map((item, index) => (
           <TouchableOpacity
-            key={item.id}
+            key={index}
             style={styles.gridItem}
             onPress={() => toggleModal(item)}
           >
-            <Text style={styles.gridText}>{item.title}</Text>
+            <Image source={item.image} style={styles.gridImage} />
           </TouchableOpacity>
         ))}
       </View>
@@ -188,7 +278,7 @@ export default function Home({ token, setToken }) {
         <Text style={styles.roomButtonText}>Join Game</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.roomButton} onPress={handleJoinRoom}>
+      <TouchableOpacity style={styles.roomButton} onPress={handleStartGame}>
         <Text style={styles.roomButtonText}>Start Game</Text>
       </TouchableOpacity>
 
@@ -202,26 +292,55 @@ export default function Home({ token, setToken }) {
         backdropTransitionOutTiming={0}
       >
         <View style={styles.modalContent}>
-          <Text style={styles.modalTitle}>Create Room</Text>
-          <Text style={styles.sectionTitle}>Choose an Artist</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Enter artist name"
-            placeholderTextColor="#666"
-            value={artist}
-            onChangeText={setArtist}
-          />
-          <View style={styles.artistContainer}>
-            {artistImage && (
-              <Image source={{ uri: artistImage }} style={styles.artistImage} />
-            )}
-            <View>
-              <Text style={styles.artistName}>{artistName}</Text>
-              <Text style={styles.artistFollowers}>
-                {artistFollowers} Followers
-              </Text>
-            </View>
-          </View>
+          {selectedItem && (
+            <Text style={styles.modalDescription}>{selectedItem.description}</Text>
+          )}
+          {selectedItem && selectedItem.search && (
+            <>
+              <Text style={styles.sectionTitle}>Choose an Artist</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter artist name"
+                placeholderTextColor="#1DB954"
+                value={artist}
+                onChangeText={setArtist}
+              />
+              <View style={styles.artistContainer}>
+                {artistImage && (
+                  <Image source={{ uri: artistImage }} style={styles.artistImage} />
+                )}
+                <View>
+                  <Text style={styles.artistName}>{artistName}</Text>
+                  <Text style={styles.artistFollowers}>
+                    {artistFollowers} Followers
+                  </Text>
+                </View>
+              </View>
+            </>
+          )}
+          {selectedItem && selectedItem.insert && (
+            <>
+              <Text style={styles.sectionTitle}>Enter Playlist Link</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter playlist link"
+                placeholderTextColor="#1DB954"
+                value={playlistLink}
+                onChangeText={setPlaylistLink}
+              />
+              <View style={styles.artistContainer}>
+                {playlistImage && (
+                  <Image source={{ uri: playlistImage }} style={styles.artistImage} />
+                )}
+                <View>
+                  <Text style={styles.artistName}>{playlistName}</Text>
+                  <Text style={styles.artistFollowers}>
+                    Created by {playlistOwner}
+                  </Text>
+                </View>
+              </View>
+            </>
+          )}
           <Text style={styles.sectionTitle}>Question Type</Text>
           <View style={styles.questionTypeContainer}>
             <TouchableOpacity
@@ -307,7 +426,7 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   gridItem: {
-    backgroundColor: "#1DB954",
+    backgroundColor: "#0b0b0b",
     width: "48%",
     height: 150,
     justifyContent: "center",
@@ -315,10 +434,10 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginVertical: 5,
   },
-  gridText: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "#000",
+  gridImage: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
   },
   roomButton: {
     backgroundColor: "#1DB954",
@@ -340,7 +459,7 @@ const styles = StyleSheet.create({
     margin: 0,
   },
   modalContent: {
-    backgroundColor: "#FFFFFF",
+    backgroundColor: "#191818",
     padding: 20,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
@@ -350,7 +469,13 @@ const styles = StyleSheet.create({
     shadowRadius: 8,
     elevation: 5,
     position: "relative",
-    marginTop: 50,
+    marginTop: 0,
+  },
+  modalDescription: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#FFF",
+    marginBottom: 20,
   },
   artistContainer: {
     flexDirection: "row",
@@ -366,38 +491,28 @@ const styles = StyleSheet.create({
   artistName: {
     fontSize: 18,
     fontWeight: "bold",
+    color: "#FFF",
   },
   artistFollowers: {
     fontSize: 16,
-    color: "#666",
-  },
-  closeButton: {
-    position: "absolute",
-    top: 10,
-    right: 20,
-    zIndex: 1,
-  },
-  closeButtonText: {
-    fontSize: 24,
-    color: "#000",
-  },
-  modalTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
+    color: "#1DB954",
   },
   sectionTitle: {
     fontSize: 18,
     fontWeight: "bold",
+    color: "#FFF",
     marginBottom: 10,
   },
   input: {
     height: 40,
-    borderColor: "#DDD",
+    borderColor: "#1DB954",
     borderWidth: 1,
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 10,
+    backgroundColor: "#0b0b0b",
+    color: "#1DB954",
+    fontWeight: "bold",
   },
   questionTypeContainer: {
     flexDirection: "row",
@@ -416,6 +531,7 @@ const styles = StyleSheet.create({
   optionText: {
     fontSize: 16,
     color: "#000",
+    fontWeight: "bold",
   },
   selectedOption: {
     backgroundColor: "#0A8F4C", // Darker green for selected option
