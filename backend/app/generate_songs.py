926036934479f619c53d25ebf145db93433ff076
Mazen Youssef <98108192+Mazen1004@users.noreply.github.com>
2024-08-03 23:50:45 -0400

    leaderboard screen and album backend images

--- backend/app/generate_songs.py
+++ backend/app/generate_songs.py
@@ -16,13 +16,16 @@ def generate_songs_artists(token: str, artist: str):
     if response.status_code == 200:
         data = response.json()
         for track in data['tracks']:
+            # Fetch the largest album cover image (first in the list)
+            album_cover = track['album']['images'][0]['url'] if track['album']['images'] else None
             songs.append({
                 "song_name": track['name'],
                 "album_name": track['album']['name'],
                 "release_date": track['album']['release_date'],
                 "popularity": track['popularity'],
                 "preview_url": track['preview_url'],  # This might be None if not available
-                "spotify_url": track['external_urls']['spotify']
+                "spotify_url": track['external_urls']['spotify'],
+                "album_cover": album_cover  # Add the album cover image URL
             })
     else:
         raise Exception(f"Failed to fetch top tracks: {response.status_code}, {response.text}")
